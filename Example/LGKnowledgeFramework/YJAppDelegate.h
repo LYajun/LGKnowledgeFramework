64bd6fb470a41466572ea6ddcc0bc0ea4c2f5295
刘亚军 <liuyajun1999@icloud.com>
2019-09-18 09:35:54 +0800

    Initial commit

---
+++ Example/LGKnowledgeFramework/YJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YJAppDelegate.h
+//  LGKnowledgeFramework
+//
+//  Created by lyj on 09/18/2019.
+//  Copyright (c) 2019 lyj. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/LGKnowledgeFramework/YJViewController.h
@@ -0,0 +1,13 @@
+//
+//  YJViewController.h
+//  LGKnowledgeFramework
+//
+//  Created by lyj on 09/18/2019.
+//  Copyright (c) 2019 lyj. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YJViewController : UIViewController
+
+@end
